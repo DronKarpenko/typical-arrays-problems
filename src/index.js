b62d38b6ef960b99c5de0b088aa8f553436557bc
Andrey <dron.donbass@gmail.com>
2021-03-08 11:33:31 +0200

    feat: typical arrays problems solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0;
+  }
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+   if(array === undefined || array.length === 0) {
+       return 0;
+   }
+   return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0;
+  }
+  let summa = 0;
+    for(let i = 0; i < array.length; i++) {
+        summa += array[i];
+    }
+    let avg = summa / array.length;
+    return avg;
 }
